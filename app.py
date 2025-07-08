25f7093f91eab173603631793fffec4d4bec1bac
Nikole <codecute9@gmail.com>
2025-07-08 12:03:15 -0700

    Creating first fun madlib!

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("This is my first repo, YAY!")
+# print("This is my first repo, YAY!")
+
+adj=input("Give me an adjective")
+noun=input("Give me noun")
+noun2=input("ANOTHER NOUN")
+noun3=input("One last one please")
+num=input("Random number, Any number")
+
+print("This morning I went outside to walk my " + noun + "." + " I saw " + noun2 + "." + "We planned to go to the " + noun3 + " together at " + num + "pm. We had a very " + adj + " time")
+
+#Testing true or false user types
+# second = input("Want to play another round?")
+
+# if yes = True print("Here is another")
+# else: print("See you next time!")
+# print(second)
